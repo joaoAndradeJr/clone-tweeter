467d490a96534fe1a6869cc8dfbf339bf98a7577
felipe <felipeavmuller@gmail.com>
2023-04-10 09:54:55 -0300

    add link 404

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './not-found.css';
 
 function NotFound() {
@@ -5,6 +6,7 @@ function NotFound() {
     <div className="not-found">
       <h1>404</h1>
       Ih, esta página não existe. Tente buscar outra coisa.
+      <Link to="/">Voltar para a página inicial.</Link>
     </div>
   );
 }
