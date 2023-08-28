221e4f54f71bce0566b6c3ddec046d95ab57df8b
Joao Andrade Jr <fullservicemanaus@gmail.com>
2023-08-28 19:41:29 -0300

    feat: req 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import NotFound from "./pages/not-found";
+
 function App() {
   return (
-    <h1>Clone Twitter</h1>
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="/profile/:username" element={ <Profile /> } />
+      <Route path="*" element={ <NotFound /> } />
+    </Routes>
   );
 }
 
